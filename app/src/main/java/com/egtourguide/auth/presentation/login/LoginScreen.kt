1104024bdc2df47e5577263940646b3689176d8f
ziad1099 <81701517+ziad1099@users.noreply.github.com>
2024-03-21 22:31:36 +0200

    test

--- app/src/main/java/com/egtourguide/auth/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/egtourguide/auth/presentation/login/LoginScreen.kt
@@ -1,6 +1,7 @@
 package com.egtourguide.auth.presentation.login
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,8 +16,12 @@ fun LoginScreen() {
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Text(text = "Login", fontSize = 28.sp, fontWeight = FontWeight.Bold)
-        Text(text = "Test", fontSize = 28.sp, fontWeight = FontWeight.Bold)
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+
+        }
 
     }
 }
