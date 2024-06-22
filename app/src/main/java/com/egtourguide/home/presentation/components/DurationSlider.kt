fd49e0fc2791d8a6100a272ef5f440d7922125ba
Ziad Helaly <zezo97736@gmail.com>
2024-06-23 00:40:27 +0300

    Filter Screen logic

--- app/src/main/java/com/egtourguide/home/presentation/components/DurationSlider.kt
+++ app/src/main/java/com/egtourguide/home/presentation/components/DurationSlider.kt
@@ -1,18 +1,23 @@
 package com.egtourguide.home.presentation.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RangeSlider
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -84,10 +89,96 @@ fun SliderBubble(offset: Dp, value: String) {
     }
 }
 
+@Composable
+fun RangeSliderWithBubble(onValueChange: (Int,Int) -> Unit ) {
+    var sliderPosition by remember { mutableStateOf(0f..15f) }
+    val sliderRange = 0f..30f
+    var showBubble by remember { mutableStateOf(false) }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp)
+        ) {
+            if (showBubble) {
+                val startBubbleOffset = (sliderPosition.start / sliderRange.endInclusive) * (280.dp) // Adjust the width value according to your Slider width
+                val endBubbleOffset = (sliderPosition.endInclusive / sliderRange.endInclusive) * (280.dp) // Adjust the width value according to your Slider width
+                RangeSliderBubble(
+                    offsetStart = startBubbleOffset,
+                    offsetEnd = endBubbleOffset,
+                    valueStart = "${sliderPosition.start.toInt()} Days",
+                    valueEnd = "${sliderPosition.endInclusive.toInt()} Days"
+                )
+            }
+        }
+        RangeSlider(
+            value = sliderPosition,
+            onValueChange = { newValue ->
+                sliderPosition = newValue
+                showBubble = true
+                onValueChange(newValue.start.toInt(),newValue.endInclusive.toInt())
+            },
+            onValueChangeFinished = {
+                showBubble = false
+            },
+            valueRange = sliderRange,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(24.dp) // Adjust the height to make the slider thicker
+        )
+    }
+}
+
+@Composable
+private fun RangeSliderBubble(offsetStart: Dp, offsetEnd: Dp, valueStart: String, valueEnd: String) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .offset(x = offsetStart - 20.dp) // Adjust for the width of the bubble
+                .clip(RoundedCornerShape(16.dp))
+                .background(MaterialTheme.colorScheme.primary)
+                .padding(8.dp)
+        ) {
+            Text(
+                text = valueStart,
+                color = MaterialTheme.colorScheme.onPrimary,
+                fontSize = 12.sp
+            )
+        }
+
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .offset(x = offsetEnd - 250.dp) // Adjust for the width of the bubble
+                .clip(RoundedCornerShape(16.dp))
+                .background(MaterialTheme.colorScheme.primary)
+                .padding(8.dp)
+        ) {
+            Text(
+                text = valueEnd,
+                color = MaterialTheme.colorScheme.onPrimary,
+                fontSize = 12.sp
+            )
+        }
+    }
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     EGTourGuideTheme {
-        DurationSlider()
+        RangeSliderWithBubble(){a,b->
+
+        }
     }
 }
--- app/src/main/java/com/egtourguide/home/presentation/screens/filter/FilterScreenViewModel.kt
+++ app/src/main/java/com/egtourguide/home/presentation/screens/filter/FilterScreenViewModel.kt
@@ -10,23 +10,213 @@ import javax.inject.Inject
 class FilterScreenViewModel @Inject constructor() : ViewModel() {
     private val _uiState = MutableStateFlow(FilterScreenState())
     val uiState = _uiState.asStateFlow()
-    private var selectedMap= hashMapOf<String,List<String>>()
+    private var selectedMap = hashMapOf<String, List<String>>()
+    private var categoryList = mutableListOf<String>()
+    private var artifactTypeList = mutableListOf<String>()
+    private var locationList = mutableListOf<String>()
+    private var ratingList = mutableListOf<String>()
+    private var materialList = mutableListOf<String>()
+    private var tourTypeList = mutableListOf<String>()
+    private var tourismTypeList = mutableListOf<String>()
+    private var sortByList = mutableListOf<String>()
+    private var queryList = mutableListOf<String>()
+    private var durationList = mutableListOf<String>()
 
-    fun changeDuration(days:Int){
-        _uiState.update { it.copy(duration = days) }
+
+
+    init {
+        _uiState.update {
+            it.copy(
+                categoryFilters = listOf("Tours", "Landmarks", "Artifacts"),
+                artifactTypeList = listOf(
+                    "Pottery",
+                    "Sculptures",
+                    "Statues",
+                    "Jewelry",
+                    "Manuscripts",
+                    "Tools",
+                    "Utensils",
+                    "Weapons",
+                    "Armor",
+                    "Textiles",
+                    "Coins",
+                    "Religious",
+                    "Artistic",
+                    "Architectural",
+                    "Decorative"
+                ),
+                materialList = listOf(
+                    "Stone", "Clay", "Wood", "Papyrus", "Marble", "Gold", "Silver",
+                    "Bronze", "Silk", "Leather", "Glass"
+                ),
+                locationFilters = listOf(
+                    "Alexandria",
+                    "Aswan",
+                    "Assiut",
+                    "Beheira",
+                    "Beni Suef",
+                    "Cairo",
+                    "Dakahlia",
+                    "Damietta",
+                    "Fayoum",
+                    "Gharbia",
+                    "Giza",
+                    "Ismailia",
+                    "Kafr el-Sheikh",
+                    "Matrouh",
+                    "Minya",
+                    "Menofia",
+                    "New Valley",
+                    "North Sinai",
+                    "Port Said",
+                    "Qualyubia",
+                    "Qena",
+                    "Red Sea",
+                    "Al-Sharqia",
+                    "Sohag",
+                    "South Sinai",
+                    "Suez",
+                    "Luxor"
+                ),
+                sortList = listOf("Featured", "Recently Added"),
+                tourismTypeFilters = listOf(
+                    "Adventure",
+                    "Ecotourism",
+                    "Cultural",
+                    "Medical",
+                    "Historical",
+                    "Culinary",
+                    "Urban",
+                    "Beach",
+                    "Sports",
+                    "Shopping",
+                    "Education", "Various"
+                ),
+                ratingFilters = listOf("5 Only", "4 & Up", "3 & Up", "2 & Up", "1 & Up"),
+                tourTypeList = listOf(
+                    "Adventure",
+                    "Ecotourism",
+                    "Cultural",
+                    "Medical",
+                    "Historical",
+                    "Culinary",
+                    "Urban",
+                    "Beach",
+                    "Sports",
+                    "Shopping",
+                    "Education"
+                )
+            )
+        }
     }
 
-    // TODO: Remove these!!
-    fun addSelectedFilters(label:String){}
-    fun removeSelectedFilter(filter: String){}
+    fun onApplyClick() {
+        selectedMap["Category"] = categoryList
+        selectedMap["Tourism Type"] = tourismTypeList
+        selectedMap["Artifact Type"] = artifactTypeList
+        selectedMap["Tour Type"] = tourTypeList
+        selectedMap["Duration"] = durationList
+        selectedMap["Location"] = locationList
+        selectedMap["Rating"] = ratingList
+        selectedMap["Sort By"] = sortByList
+        selectedMap["Material"] = materialList
+        selectedMap["Query"] = queryList
+        _uiState.update { it.copy(isSuccess = true) }
+    }
 
-//    fun addSelectedFilters(label:String,filter:String){
-//        selectedMap.put(label,filter)
-//        _uiState.update { it.copy(selected =selectedList ) }
-//    }
-//    fun removeSelectedFilter(filter: String){
-//        selectedList.remove(filter)
-//        _uiState.update { it.copy(selected =selectedList ) }
-//    }
+    fun onResetClick() {
+        _uiState.update { it.copy(reset = true) }
+    }
+    fun clearSuccess() {
+        _uiState.update { it.copy(isSuccess = false) }
+    }
+    fun tourScreen(){
+        _uiState.update { it.copy(isTours = true) }
+    }
+    fun landmarkScreen(){
+        _uiState.update { it.copy(isLandmarks = true) }
+    }
+    fun artifactScreen(){
+        _uiState.update { it.copy(isArtifacts = true) }
+    }
+
+    fun changeDuration(startDay: Int,endDay:Int) {
+        durationList.clear()
+        durationList.add(startDay.toString())
+        durationList.add(endDay.toString())
+    }
+
+    fun addSelectedCategoryFilter(label: String) {
+        categoryList.clear()
+        categoryList.add(label)
+        when (label) {
+            "Tours" -> _uiState.update { it.copy(isTours = true) }
+            "Landmarks" -> _uiState.update { it.copy(isLandmarks = true) }
+            "Artifacts" -> _uiState.update { it.copy(isArtifacts = true) }
+        }
+    }
+
+    fun saveQuery(query: String) {
+        queryList.add(query)
+    }
+
+    fun addSelectedLocationFilter(label: String) {
+        locationList.add(label)
+    }
+
+    fun removeSelectedLocationFilter(label: String) {
+        locationList.remove(label)
+    }
+
+    fun addSelectedTourTypeFilter(label: String) {
+        tourTypeList.add(label)
+    }
+
+    fun removeSelectedTourTypeFilter(label: String) {
+        tourTypeList.remove(label)
+    }
+
+    fun addSelectedMaterialFilter(label: String) {
+        materialList.add(label)
+    }
+
+    fun removeSelectedMaterialFilter(label: String) {
+        materialList.remove(label)
+    }
+
+    fun addSelectedArtifactTypeFilter(label: String) {
+        artifactTypeList.add(label)
+    }
+
+    fun removeSelectedArtifactTypeFilter(label: String) {
+        artifactTypeList.remove(label)
+    }
+
+    fun addSelectedRatingFilter(label: String) {
+        ratingList.clear()
+        ratingList.add(label.first().toString())
+    }
+
+    fun removeSelectedRatingFilter(label: String) {
+        ratingList.clear()
+    }
+
+    fun addSelectedSortByFilter(label: String) {
+        sortByList.clear()
+        sortByList.add(label)
+    }
+
+    fun removeSelectedSortByFilter(label: String) {
+        sortByList.clear()
+    }
+
+    fun addSelectedTourismTypeFilter(label: String) {
+        tourismTypeList.add(label)
+    }
+
+    fun removeSelectedTourismTypeFilter(label: String) {
+        tourismTypeList.remove(label)
+    }
+
 
 }
