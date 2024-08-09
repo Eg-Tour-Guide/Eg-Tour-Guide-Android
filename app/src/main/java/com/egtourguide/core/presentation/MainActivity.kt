5aeb317b550521a982093d1892caee7d7234d8cc
Abdo Sharaf <bodyasharaf8@gmail.com>
2024-08-10 01:20:58 +0300

    Create a generic PullToRefreshScreen.

--- app/src/main/java/com/egtourguide/core/presentation/MainActivity.kt
+++ app/src/main/java/com/egtourguide/core/presentation/MainActivity.kt
@@ -3,32 +3,6 @@ package com.egtourguide.core.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
-import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
@@ -36,13 +10,11 @@ import com.egtourguide.core.domain.usecases.GetFromDataStoreUseCase
 import com.egtourguide.core.presentation.navigation.AppGraph
 import com.egtourguide.core.presentation.navigation.AppNavigation
 import com.egtourguide.core.presentation.ui.theme.EGTourGuideTheme
-import com.egtourguide.core.utils.Constants.TAG
 import com.egtourguide.core.utils.DataStoreKeys.IS_LOGGED_KEY
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -67,23 +39,10 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             EGTourGuideTheme {
-//                val navController = rememberNavController()
-//                AppNavigation(
-//                    navController = navController,
-//                    startDestination = getStartDestination()
-//                )
-                var isRefreshing by remember { mutableStateOf(false) }
-                val scope = rememberCoroutineScope()
-
-                PullToRefresh(
-                    isRefreshing = isRefreshing,
-                    onRefresh = {
-                        scope.launch {
-                            isRefreshing = true
-                            delay(2000L)
-                            isRefreshing = false
-                        }
-                    }
+                val navController = rememberNavController()
+                AppNavigation(
+                    navController = navController,
+                    startDestination = getStartDestination()
                 )
             }
         }
@@ -99,68 +58,5 @@ class MainActivity : ComponentActivity() {
     private fun getStartDestination(): String {
         return if (isLogged == true) AppGraph.Main.route
         else AppGraph.Auth.route
-    }
-}
-
-@Preview
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun PullToRefresh(
-    isRefreshing: Boolean = false,
-    onRefresh: () -> Unit = {}
-) {
-    val pullToRefreshState = rememberPullToRefreshState()
-
-    Box(
-        modifier = Modifier
-            .background(MaterialTheme.colorScheme.background)
-            .nestedScroll(pullToRefreshState.nestedScrollConnection)
-    ) {
-        Column(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Text(
-                text = "Hello!",
-                style = MaterialTheme.typography.displayMedium,
-                color = MaterialTheme.colorScheme.onBackground,
-                modifier = Modifier.padding(16.dp)
-            )
-
-            LazyColumn(
-                modifier = Modifier.weight(1f),
-                contentPadding = PaddingValues(16.dp),
-                verticalArrangement = Arrangement.spacedBy(16.dp)
-            ) {
-                items(20) {
-                    Text(
-                        text = "Item #$it",
-                        style = MaterialTheme.typography.bodyMedium,
-                        color = MaterialTheme.colorScheme.onBackground,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 16.dp)
-                    )
-                }
-            }
-        }
-
-        if (pullToRefreshState.isRefreshing) {
-            LaunchedEffect(true) {
-                Timber.tag(TAG).d("PullToRefresh: Refreshing")
-                onRefresh()
-            }
-        }
-
-        LaunchedEffect(isRefreshing) {
-            if (isRefreshing) pullToRefreshState.startRefresh()
-            else pullToRefreshState.endRefresh()
-        }
-
-        PullToRefreshContainer(
-            state = pullToRefreshState,
-            containerColor = MaterialTheme.colorScheme.primary,
-            contentColor = MaterialTheme.colorScheme.onPrimary,
-            modifier = Modifier.align(Alignment.TopCenter)
-        )
     }
 }
---
+++ app/src/main/java/com/egtourguide/core/presentation/components/PullToRefreshScreen.kt
@@ -0,0 +1,118 @@
+package com.egtourguide.core.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.egtourguide.core.presentation.ui.theme.EGTourGuideTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@Preview
+@Composable
+private fun PullToRefreshScreenPreview() {
+    EGTourGuideTheme {
+        var isRefreshing by remember { mutableStateOf(false) }
+        val scope = rememberCoroutineScope()
+
+        PullToRefreshScreen(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background),
+            isRefreshing = isRefreshing,
+            onRefresh = {
+                scope.launch {
+                    isRefreshing = true
+                    delay(2000L)
+                    isRefreshing = false
+                }
+            }
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Text(
+                    text = "Hello!",
+                    style = MaterialTheme.typography.displayMedium,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    modifier = Modifier.padding(16.dp)
+                )
+
+                LazyColumn(
+                    modifier = Modifier.weight(1f),
+                    contentPadding = PaddingValues(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    items(20) {
+                        Text(
+                            text = "Item #$it",
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = MaterialTheme.colorScheme.onBackground,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 16.dp)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PullToRefreshScreen(
+    modifier: Modifier = Modifier,
+    isRefreshing: Boolean,
+    onRefresh: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    val pullToRefreshState = rememberPullToRefreshState()
+
+    Box(
+        modifier = modifier
+            .nestedScroll(pullToRefreshState.nestedScrollConnection)
+    ) {
+        content()
+
+        if (pullToRefreshState.isRefreshing) {
+            LaunchedEffect(true) {
+                onRefresh()
+            }
+        }
+
+        LaunchedEffect(isRefreshing) {
+            if (isRefreshing) pullToRefreshState.startRefresh()
+            else pullToRefreshState.endRefresh()
+        }
+
+        PullToRefreshContainer(
+            state = pullToRefreshState,
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = MaterialTheme.colorScheme.onPrimary,
+            modifier = Modifier.align(Alignment.TopCenter)
+        )
+    }
+}
